786812fc4991c964b0760a7e4ac3e6a22e89f0a7
Cristian David Henao Vergel <cristian.1701823811@ucaldas.edu.co>
2021-01-29 17:52:10 -0500

    creacion del tablero

--- index.php
+++ index.php
@@ -4,48 +4,48 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/png" href="pix/favico.png">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <link href="css/login.css" rel="stylesheet" >
-        <script src="js/ajax.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <title>Login antropolys</title>
-        <style>
-        </style>
-    </head>
-    <body id="body_index">
-        
-        <div class="container">
-            <div class="row">
-                <div class="container" id="formContainer">
-
-                    <form method='post' class="form-signin" id="login" role="form">
-                        <label for="user"><b>Usuario</b></label>
-                        <input type="text" class="form-control" name="user" id="user" placeholder="user" required autofocus>
-                        <label for="pass"><b>Clave</b></label>
-                        <input type="password" class="form-control" name="pass" id="celular" placeholder="***" required>
-                        <div class="col-sm-6" id="dados_login"></div>
-                        <div class="col-sm-6" id="btn_login">
-                            <button class="btn btn-lg btn-primary btn-block" type="submit" id="btn_entrar">Entrar</button>
-                        </div>
-                    </form>
-                </div> <!-- /container -->
-            </div>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="resources/assets/pix/favico.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="resources/css/login.css" rel="stylesheet" >
+    <!-- <script src="js/ajax.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Login antropolys</title>
+</head>
+<body id="body_index">
+    
+    <div class="container">
+		<img id="img_login" src="resources/assets/imagenes_nuevas/PNG/login_letrero.png"> 
+        <div class="row">
+            <div class="container" id="formContainer">
+                
+                <form method='post' class="form-signin text-center" id="login" role="form">
+                    <label for="user">Usuario</label>
+                    <div class="form-group" id="user_group">
+                        <input type="text" class="form-control" name="user" id="user" required autofocus>
+                        <img src="resources/assets/imagenes_nuevas/PNG/estrella.png" id="img_icon1">
+                    </div>
+                    <label for="pass">Clave</label>
+                    <div class="form-group" id="celular_group">
+                        <input type="password" class="form-control" name="pass" id="celular"  required>
+                        <img src="resources/assets/imagenes_nuevas/PNG/reloj.png" id="img_icon2">
+                    </div>
+                    <a id="link_olvidoClave" href="#"><b>¿Olvidó su clave?</b></a>      
+                    <br>
+                    <button class="btn btn-block" type="submit" id="btn_entrar"></button>
+                    <button class="btn btn-block" type="submit" id="btn_registro"></button>
+                </form>
+                <img id="img_logo" src="resources/assets/imagenes_nuevas/PNG/Logo.png"> 
+            </div> <!-- /container -->
+            
         </div>
-        <?php
-
-        if($_POST && array_key_exists('celular', $_POST)){
-            $cel = $_POST['celular'];
-            echo '<script> ajax_b.query_users('.$cel.');</script>';
-            
-        }
-        // put your code here
-        ?>
-            
-    </body>
+        <button class="btn btn-block" type="submit" id="btn_entrar1"></button>
+        <button class="btn btn-block" type="submit" id="btn_registro1"></button>
+    </div>
+    
+</body>
 </html>
---
+++ resources/views/tablero.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="../../resources/assets/pix/favico.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="../../resources/css/tablero.css" rel="stylesheet" >
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Tablero antropolys</title>
+</head>
+<body id="body_tablero">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-2 estancia_1"> 
+                <div class="col-xs-6 content_img" id=""> <img src="../assets/Tablero-tajado/Mesa-de-trabajo20-100.jpg"> </div> 
+                <div class="col-xs-6 content_streetVertical" id=""> <img src="../assets/Tablero-tajado/calle-vertical.png">  </div>
+            </div>
+            <div class="col-xs-2 estancia_2"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo27-100.jpg">  </div>
+                <div class="col-xs-6 content_streetVertical" id=""> <img src="../assets/Tablero-tajado/calle-vertical.png"> </div>
+            </div>
+            <div class="col-xs-2 estancia_3"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo26-100.jpg">  </div>
+            </div>
+            <div class="col-xs-2 estancia_4"> 
+                <div class="col-xs-6 content_img" id=""> <img src="../assets/Tablero-tajado/Mesa-de-trabajo20-100.jpg"> </div> 
+                <div class="col-xs-6 content_streetVertical" id="">  </div>
+            </div>
+            <div class="col-xs-2 estancia_5"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo30-100.jpg">  </div>
+                <div class="col-xs-6 content_streetVertical" id="">  </div>
+            </div>
+            <div class="col-xs-2 estancia_6"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo26-100.jpg">  </div>
+            </div>
+            <div class="content_streetHorizontal"> <img src="../assets/Tablero-tajado/der-.png"> </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-2 estancia_7"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo21-100.jpg">  </div>
+                <div class="col-xs-6 content_streetVertical" id="">  </div>
+            </div>
+            <div class="col-xs-2 estancia_8"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo28-100.jpg">  </div>
+                <div class="col-xs-6 content_streetVertical" id="">  </div>
+            </div>
+            <div class="col-xs-2 estancia_9"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo25-100.jpg">  </div>
+            </div>
+            <!-- <div class="col-xs-2 estancia_10"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo21-100.jpg">  </div>
+                <div class="col-xs-6 content_streetVertical" id="">  </div>
+            </div>
+            <div class="col-xs-2 estancia_11"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo28-100.jpg">  </div>
+                <div class="col-xs-6 content_streetVertical" id="">  </div>
+            </div>
+            <div class="col-xs-2 estancia_12"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo25-100.jpg">  </div>
+            </div> -->
+            <div class="content_streetHorizontal" id="">  </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-2 estancia_13"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo22-100.jpg">  </div>
+            </div>
+            <div class="col-xs-2 estancia_14"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg">  </div>
+                <div class="col-xs-6 content_streetVertical" id="">  </div>
+            </div>
+            <div class="col-xs-2 estancia_15"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo24-100.jpg">  </div>
+            </div>
+            <!-- <div class="col-xs-2 estancia_16"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo22-100.jpg">  </div>
+            </div>
+            <div class="col-xs-2 estancia_17"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg">  </div>
+                <div class="col-xs-6 content_streetVertical" id="">  </div>
+            </div>
+            <div class="col-xs-2 estancia_18"> 
+                <div class="col-xs-6 content_img"> <img src="../assets/Tablero-tajado/Mesa-de-trabajo24-100.jpg">  </div>
+            </div> -->
+        </div>
+    </div>
+</body>
+</html>
