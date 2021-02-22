4a2abe0c1f71ab7d0f67235264c902ac37129cf3
Cristian David Henao Vergel <cristian.1701823811@ucaldas.edu.co>
2021-02-21 20:31:31 -0500

    adecuacion tablero

--- resources/js/preguntas.js
+++ resources/js/preguntas.js
@@ -3,23 +3,22 @@ class preguntas {
 
     save_questions(user){
         var formData = pre.validacion_campos('question');
-        // if(formData){
-        //     formData.append("key", "C3");
-        //     ////Enviar objeto a crear
-        //     var xhttp = new XMLHttpRequest();
-        //     xhttp.onreadystatechange = function() {
-        //         if (this.readyState == 4 && this.status == 200) {
-        //             var fjson = JSON.parse(this.responseText);
-        //             if(fjson.ack){
-        //                 console.log("estoaqui")
-        //                 msjBC.ok('Hola','Pregunta Ingresada'); 
+        if(formData){
+            formData.append("key", "C3");
+            ////Enviar objeto a crear
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    var fjson = JSON.parse(this.responseText);
+                    if(fjson.ack){
+                        msjBC.ok('Hola','Pregunta Ingresada'); 
                         
-        //             }else msjBC.informacion('ERROR',fjson.respuesta); 
-        //         }
-        //     };
-        //     xhttp.open("post", '../../../class/conexion.php', true);
-        //     xhttp.send(formData);
-        // }
+                    }else msjBC.informacion('ERROR',fjson.respuesta); 
+                }
+            };
+            xhttp.open("post", '../../../class/conexion.php', true);
+            xhttp.send(formData);
+        }
     }
 
     validacion_campos(form){
--- resources/views/tablero.php
+++ resources/views/tablero.php
@@ -12,39 +12,192 @@
     <title>Tablero antropolys</title>
 </head>
 <body class="bodyTablero" id="body_tablero">
-    <div class="container" id="">
-        <div class="row">
-            <div class="col-xs-12 content_img1">
-                <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo20-100.jpg"> </div>
-                <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical1.png"> </div>
-                <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo27-100.jpg"> </div>
-                <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical1.png"> </div>
-                <div class="col-xs-2 img3"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo26-100.jpg"> </div>            
+    <div class="container">
+        <div class="row tableroNormal" id="tablero_normal">
+            <div class="tablero1">
+                <div class="col-xs-12 content_img1">
+                    <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo22-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical_1.png"> </div>
+                    <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo21-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical_1.png"> </div>
+                    <div class="col-xs-2 img3"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo20-100.jpg"> </div>            
+                </div>
+                <div class="col-xs-12 content_imgStreetH"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH1"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img2">
+                    <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo28-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-2 img3"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo27-100.jpg"> </div>             
+                </div>
+                <div class="col-xs-12 content_imgStreetH2"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH3"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img3">
+                    <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical3.jpg"> </div>
+                    <div class="col-xs-4 img2"> 
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo25-100.jpg">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo21-100.jpg">
+                    </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical3.jpg"> </div>
+                    <div class="col-xs-2 img3">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo26-100.jpg">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo20-100.jpg">
+                    </div>             
+                </div>
             </div>
-
-            <div class="row">
-                <div class="col-xs-12 content_streetH1">
-                    <div class="col-xs-4 img1"> <img src="../../resources/assets/Tablero-tajado/der-.png"> </div>
-                    <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.png"> </div>
-                    <div class="col-xs-4 img3"> <img src="../../resources/assets/Tablero-tajado/_izq.png"> </div>
-                </div>                
+            <div class="tablero2">
+                <div class="col-xs-12 content_imgStreetH"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH1"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img2">
+                    <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo29-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-2 img3"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo30-100.jpg"> </div>             
+                </div>
+                <div class="col-xs-12 content_imgStreetH2"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH3"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img3">
+                    <div class="col-xs-2 img1"> 
+                        <div class="calle"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal1.png"> </div>
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg">
+                    </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical3.jpg"> </div>
+                    <div class="col-xs-4 img2"> 
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo25-100.jpg">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo21-100.jpg">
+                    </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical3.jpg"> </div>
+                    <div class="col-xs-2 img3">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo26-100.jpg">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo20-100.jpg">
+                    </div>             
+                </div>
             </div>
-            <div class="col-xs-12 content_img2">
-                <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo21-100.jpg"> </div>
-                <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.png"> </div>
-                <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo28-100.jpg"> </div>
-                <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.png"> </div>
-                <div class="col-xs-2 img3"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo25-100.jpg"> </div>            
-            </div>
-
-            <div class="row">
-                <div class="col-xs-12 content_streetH2">
-                    <div class="col-xs-4 img1"> <img src="../../resources/assets/Tablero-tajado/der-.png"> </div>
-                    <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.png"> </div>
-                    <div class="col-xs-4 img3"> <img src="../../resources/assets/Tablero-tajado/_izq.png"> </div>
-                </div>                
-            </div>
+            <div class="tablero3">
+                <div class="col-xs-12 content_imgStreetH"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH1"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img2">
+                    <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo32-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-2 img3"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo31-100.jpg"> </div>             
+                </div>
+                <div class="col-xs-12 content_imgStreetH2"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH3"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img3">
+                    <div class="col-xs-2 img1"> 
+                        <div class="calle"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal1.png"> </div>
+                        <img class="opcion" src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo22-100.jpg">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg">
+                    </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical3.jpg"> </div>
+                    <div class="col-xs-4 img2"> 
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo25-100.jpg">
+                    </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical3.jpg"> </div>
+                    <div class="col-xs-2 img3">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo26-100.jpg">
+                    </div>             
+                </div>
+            </div>                   
         </div>
+
+        <div class="row tableroLargo" id="tablero_largo">
+            <div class="tablero1">
+                <div class="col-xs-12 content_img1">
+                    <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo22-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical_1.png"> </div>
+                    <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo21-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical_1.png"> </div>
+                    <div class="col-xs-2 img3"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo20-100.jpg"> </div>            
+                </div>
+                <div class="col-xs-12 content_imgStreetH"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH1"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img2">
+                    <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo33-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-2 img3"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo34-100.jpg"> </div>             
+                </div>
+                <div class="col-xs-12 content_imgStreetH2"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH3"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img3">
+                    <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical3.jpg"> </div>
+                    <div class="col-xs-4 img2"> 
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo25-100.jpg">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo21-100.jpg">
+                    </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical3.jpg"> </div>
+                    <div class="col-xs-2 img3">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo26-100.jpg">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo20-100.jpg">
+                    </div>             
+                </div>
+            </div>
+            <div class="tablero2">
+                <div class="col-xs-12 content_imgStreetH"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH1"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img2">
+                    <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo36-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-2 img3"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo35-100.jpg"> </div>             
+                </div>
+                <div class="col-xs-12 content_imgStreetH2"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH3"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img3">
+                    <div class="col-xs-2 img1"> 
+                        <div class="calle"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal1.png"> </div>
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg">
+                    </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical3.jpg"> </div>
+                    <div class="col-xs-4 img2"> 
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo25-100.jpg">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo21-100.jpg">
+                    </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical3.jpg"> </div>
+                    <div class="col-xs-2 img3">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo26-100.jpg">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo20-100.jpg">
+                    </div>             
+                </div>
+            </div>
+            <div class="tablero3">
+                <div class="col-xs-12 content_imgStreetH"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH1"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img2">
+                    <div class="col-xs-2 img1"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo23-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-4 img2"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo37-100.jpg"> </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical2.jpg"> </div>
+                    <div class="col-xs-2 img3"> <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo38-100.jpg"> </div>             
+                </div>
+                <div class="col-xs-12 content_imgStreetH2"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal2.jpg"> </div>
+                <div class="col-xs-12 content_imgStreetH3"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal4.jpg"> </div>
+                <div class="col-xs-12 content_img3">
+                    <div class="col-xs-2 img1"> 
+                        <div class="calle"> <img src="../../resources/assets/Tablero-tajado/calle-horizontal1.png"> </div>
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo22-100.jpg">
+                    </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical_2.png"> </div>
+                    <div class="col-xs-4 img2"> 
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo25-100.jpg">
+                    </div>
+                    <div class="col-xs-2 imgStreet"> <img src="../../resources/assets/Tablero-tajado/calle-vertical_2.png"> </div>
+                    <div class="col-xs-2 img3">
+                        <img src="../../resources/assets/Tablero-tajado/Mesa-de-trabajo26-100.jpg">
+                    </div>             
+                </div>
+            </div>               
+        </div>
+
     </div>
 </body>
 </html>
