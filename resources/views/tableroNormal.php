8d4f422d6fc971e21aa32e9cbcf9fbce149652c0
danisierravergel@gmail.com <danisierravergel@gmail.com>
2021-03-08 21:22:08 -0500

    img tableros

--- resources/views/tableroNormal.php
+++ resources/views/tableroNormal.php
@@ -33,6 +33,7 @@
     $ciudad = $_SESSION['data_user_antropolys']['ciudad'];
     $genero = $_SESSION['data_user_antropolys']['genero'];
     $correo = $_SESSION['data_user_antropolys']['email'];
+    $img = $_SESSION['data_user_antropolys']['img']? 'avatar_0'.$_SESSION['data_user_antropolys']['img']: 'avatar_principal';
 
     $icono = ($genero == "M") ? "<i class='fa fa-mars genero'></i>" : "<i class='fa fa-venus genero'></i>";
 ?>
@@ -398,7 +399,7 @@
         </div>
         
         <div class="perfilDados" id="dados">
-            <div class="btn btnUsuario avatarPrincipal" id="btnUsuario" type="submit" data-toggle="collapse" data-target="#perfilUsuarioModal"  aria-expanded="true"> </div>
+            <div class="btn btnUsuario avatarPrincipal" id="btnUsuario" type="submit" data-toggle="collapse" data-target="#perfilUsuarioModal"  aria-expanded="true" style="background-image: url(../assets/imagenes_nuevas/PNG/<?php echo $img ?>.png)"> </div>
             <div class="btn btnDados" type="submit" id="btnDados"> </div>
             
             <div class="modal fade collapse" tabindex="-1" role="dialog" id="perfilUsuarioModal" aria-expanded="true" style="">
@@ -409,7 +410,7 @@
                         <div class="row">
                             <div class="container contentPerfil" id="content_perfil">
                                 <div class="bloque">
-                                    <div class="imgAvatar avatarPrincipal"></div>
+                                    <div class="imgAvatar avatarPrincipal" style="background-image: url(../assets/imagenes_nuevas/PNG/<?php echo $img ?>.png)"></div>
                                     <div class="btn avatar" type="submit" id="btn_avatar" data-toggle="collapse" data-target="#avatarModal"  aria-expanded="true" onclick="document.getElementById('perfilUsuarioModal').classList.remove('in');"> </div>
                                     
                                     <div class="info">
