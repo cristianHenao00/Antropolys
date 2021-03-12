18baf50fb8003edd474c6f7a4abd8ec409af648d
Cristian David Henao Vergel <cristian.1701823811@ucaldas.edu.co>
2021-03-12 17:09:38 -0500

    normal

--- resources/views/tableroNormal.php
+++ resources/views/tableroNormal.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <!--<link href="../../resources/css/tableroNormal.css" rel="stylesheet" >-->
+    <link href="../../resources/css/tableroNormal.css" rel="stylesheet" >
     <link href="../../resources/css/tableroLargo.css" rel="stylesheet" >
     <link href="../../resources/css/perfilUsuario.css" rel="stylesheet" >
     <link href="../../resources/css/avatar.css" rel="stylesheet" >
