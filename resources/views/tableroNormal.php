e313a991d765e06e80503d0ad0b03bf7f3c062ee
Cristian David Henao Vergel <cristian.1701823811@ucaldas.edu.co>
2021-04-06 07:43:14 -0500

    Faltaba arreglar el tablero normal

--- resources/views/tableroNormal.php
+++ resources/views/tableroNormal.php
@@ -500,6 +500,51 @@
                 </div>
            </div>
         </div>
+
+        <div class="ganaste modal fade collapse" tabindex="-1" role="dialog" id="modal_ganaste" aria-expanded="true" style="">
+           <div class="content_modal">
+                <div class="container ganaste" >
+                    <img class="imgTerminado" src="../../resources/assets/imagenes_nuevas/PNG/tabla_terminado.png">
+                    <div class="container content_ganaste" id="contentGanaste">
+                        <div class="imgGanaste"></div>
+                        <div class="row" id="content_avatar">
+                            <div class="col-xs-1 segundo avatar0" id="">
+                                <div class="numero imgSegundo"></div>
+                                <div class="infoUsuario">
+                                    <div id="nombreUsuario2">Sandra</div>
+                                    <div class="apellido" id="apellidoUsuario2">Montoya</div>
+                                </div>
+                            </div>
+                            <div class="col-xs-1 primero avatar0" id="">
+                                <div class="numero imgPrimero"></div>
+                                <div class="infoUsuario tmp">
+                                    <div id="nombreUsuario1">Sandra</div>
+                                    <div class="apellido" id="apellidoUsuario1">Montoya</div>
+                                </div>
+                            </div>
+                            <div class="col-xs-1 tercero avatar0" id="">
+                                <div class="numero imgTercero"></div>
+                                <div class="infoUsuario">
+                                    <div id="nombreUsuario3">Sandra</div>
+                                    <div class="apellido" id="apellidoUsuario3">Montoya</div>
+                                </div>
+                            </div> 
+                        </div>
+                        <div class="btn btnNuevoJuego" id="btn_nuevoJuego" type="submit" onclick="document.getElementById('modal_ganaste').classList.remove('in');""></div>
+                    </div>
+                </div>
+           </div>
+        </div>
+
+        <div class="juguemos modal" id="mjuguemos">
+            <div class="container juguemos" >
+                <div class="container content_juguemos" id="contentJuguemos">
+                    <div class="imgJuguemos"></div>
+                    <div class="txtJuguemos" id="txt_juguemos"></div>                        
+                </div>
+            </div>
+        </div>
+
     </div>
 </body>
 </html>
